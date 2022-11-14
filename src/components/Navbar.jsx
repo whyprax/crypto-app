08ed90011f14979ab753bd43437e8d7074f4a08a
prathamdubey <prathamd67@gmail.com>
2022-11-14 20:35:56 +0530

    Dependencies Installed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar
+          src="https://raw.githubusercontent.com/adrianhajdin/project_cryptoverse/main/src/images/cryptocurrency.png"
+          size="large"
+        />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        <Button className="menu-control-container"></Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
