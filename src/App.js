acb97c2e68bf8019e17d1d52c680e2d91fabdcdd
prathamdubey <prathamd67@gmail.com>
2022-11-15 00:51:21 +0530

    Layout Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+} from "./components";
 import "./App.css";
+import { Layout, Typography, Space } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -8,8 +17,34 @@ function App() {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer" level={5}>
+          <Typography.Title style={{ color: "white", display: "flex" }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
+
+const Homepage = () => {
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+    </>
+  );
+};
+
+export default Homepage;
